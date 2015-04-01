e4fda5cc591976273698d0a7ad7db5242fb807db
Thomas Pronold <tp@tpronold.de>
2015-04-01 15:17:20 +0200

    initial commit

---
+++ android/src/de/tomgrill/gdxtwitter/app/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.tomgrill.gdxtwitter.app.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.tomgrill.gdxtwitter.app.GdxTwitterSampleApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxTwitterSampleApp(), config);
+	}
+}
---
+++ desktop/src/de/tomgrill/gdxtwitter/app/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.tomgrill.gdxtwitter.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.tomgrill.gdxtwitter.app.GdxTwitterSampleApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxTwitterSampleApp(), config);
+	}
+}
