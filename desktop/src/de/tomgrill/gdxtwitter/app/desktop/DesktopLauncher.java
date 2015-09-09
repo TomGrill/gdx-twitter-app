f7f1da1ae10f538b97cb400189ff6baa5ab6273a
Thomas Pronold <tp@tpronold.de>
2015-09-09 16:01:28 +0200

    .gitignore update

--- desktop/src/de/tomgrill/gdxtwitter/app/desktop/DesktopLauncher.java
+++ desktop/src/de/tomgrill/gdxtwitter/app/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package de.tomgrill.gdxtwitter.app.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import de.tomgrill.gdxtwitter.app.GdxTwitterSampleApp;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.height = 800;
+		config.width = 600;
 		new LwjglApplication(new GdxTwitterSampleApp(), config);
 	}
 }
